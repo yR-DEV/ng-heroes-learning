01b02b539c44ab6987dfeb63e2ce6d9de772bacc
yR-DEV <claar.zack@gmail.com>
2020-05-19 12:24:53 -0600

    Create protractor test to check the title of the webpage and it is working on both firefox and chrome

--- src/app/specs/conf.js
+++ src/app/specs/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: [],
+    specs: ['spec.js'],
     multiCapabilities: [{
         browserName: 'firefox'
     }, {
--- src/app/specs/spec.js
+++ src/app/specs/spec.js
@@ -4,4 +4,8 @@ describe('Angular Tour of Heroes App', function() {
     beforeEach(function() {
         browser.get('http://localhost:4200');
     });
+
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('AngularTourOfHeroes');
+    });
 });
